1e28a6613ac017836a2e1f50510ea51cb1a02335
Rohit Tewari <rtewari056@gmail.com>
2022-10-14 18:42:53 +0530

    Connect MongoDB to Node JS App

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const chats = require("./dummy_data");
+const connectToMongoDB = require("./config/db");
 
 const app = express();
 dotenv.config();
+connectToMongoDB();
 
 app.get("/", (req, res) => {
   res.send("API is running");
