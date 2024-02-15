71045aacb481025b260ca0ba662f3ee11e75c7f0
Rohit Tewari <Rohit.Tewari@zediant.com>
2024-02-15 12:23:35 +0530

    Initilaize Next js project with TypeScript, SCSS and Tailwind CSS

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.scss";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Bit Chat",
+  description: "BitChat is a real-time chat application made using Next js with user authentication. It allows multiple users to have a private and group chat.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
