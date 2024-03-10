954e80021dc895ed9c29368a1a8a537b568bc3b2
Rohit Tewari <Rohit.Tewari@zediant.com>
2024-03-10 21:04:05 +0530

    Add api proxy in frontend

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,13 +1,23 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'daisyui.com'
-          },
-        ]
-      }
+  // Using Next.js Rewrites to Create an API Proxy
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.API_URL}/api/:path*`,
+      },
+    ]
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'daisyui.com'
+      },
+    ]
+  }
 };
 
 export default nextConfig;
